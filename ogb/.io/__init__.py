201077eb280315ecdb4457a4fe5ba0eaf95c53d8
Zahra-Bakhtiari <zabakhti@netdev4-westus2-v2.phx.gbl>
2021-12-09 08:10:09 +0000

    bug fix

--- ogb/.io/__init__.py
+++ ogb/.io/__init__.py
@@ -1,1 +1,10 @@
+import logging
 from .save_dataset import DatasetSaver
+
+logger = logging.getLogger(__name__)
+
+try:
+    from .read_graph_pyg import read_graph_pyg, read_heterograph_pyg
+except Exception as e:
+    logger.info(f'Exception: {e}')
+    pass
--- ogb/.utils/__init__.py
+++ ogb/.utils/__init__.py
@@ -1,4 +1,21 @@
+import logging
+logger = logging.getLogger(__name__)
+
 try:
     from .mol import smiles2graph
-except ImportError:
+except Exception as e:
+    logger.info(f'Exception: {e}')
     pass
+
+try:
+    from .torch_util import replace_numpy_with_torchtensor
+except Exception as e:
+    logger.info(f'Exception: {e}')
+    pass
+
+try:
+    from .url import decide_download, download_url, extract_zip
+except Exception as e:
+    logger.info(f'Exception: {e}')
+    pass
+
